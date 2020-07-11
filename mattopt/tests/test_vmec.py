ea546dc94fce0e8bee070215f1181688295316ec
Matt Landreman <mattland@umd.edu>
2020-07-11 08:37:38 -0400

    Added more attributes to Vmec class

--- mattopt/tests/test_vmec.py
+++ mattopt/tests/test_vmec.py
@@ -14,6 +14,13 @@ class VmecTests(unittest.TestCase):
         self.assertTrue(v.stelsym.val)
         self.assertEqual(v.mpol.val, 1)
         self.assertEqual(v.ntor.val, 0)
+        self.assertEqual(v.delt.val, 0.7)
+        self.assertEqual(v.tcon0.val, 2.0)
+        self.assertEqual(v.phiedge.val, 1.0)
+        self.assertEqual(v.curtor.val, 0.0)
+        self.assertEqual(v.gamma.val, 0.0)
+        self.assertEqual(v.ncurr, 1)
+        self.assertFalse(v.free_boundary)
         self.assertEqual(v.status, "setup")
 
     def test_repr(self):
@@ -23,6 +30,25 @@ class VmecTests(unittest.TestCase):
         v = Vmec()
         self.assertEqual(v.__repr__(), \
                              "Vmec instance (nfp=1 mpol=1 ntor=0 status=setup)")
+
+    def test_parse_namelist_var(self):
+        """
+        Try adding a variable from an input namelist to a Vmec instance.
+        """
+        v = Vmec()
+        dict = {"foo":7, "bar":8, "oof":False}
+        # Try a variable that IS in the namelist:
+        v.parse_namelist_var(dict, "foo", 12)
+        self.assertEqual(v.foo.val, 7)
+        # Try a variable that is NOT in the namelist:
+        v.parse_namelist_var(dict, "zzz", 13)
+        self.assertEqual(v.zzz.val, 13)
+        # Try renaming a variable:
+        v.parse_namelist_var(dict, "bar", -7, new_name="blorp")
+        self.assertEqual(v.blorp.val, 8)
+        # Try a variable that is not a parameter:
+        v.parse_namelist_var(dict, "nerp", -5, parameter=False)
+        self.assertEqual(v.nerp, -5)
 
     def test_from_input_file(self):
         """
@@ -44,6 +70,13 @@ class VmecTests(unittest.TestCase):
         self.assertTrue(v.stelsym.val)
         self.assertEqual(v.mpol.val, 9)
         self.assertEqual(v.ntor.val, 5)
+        self.assertEqual(v.delt.val, 0.9)
+        self.assertEqual(v.tcon0.val, 2.0)
+        self.assertAlmostEqual(v.phiedge.val, 0.514386, places=13)
+        self.assertAlmostEqual(v.curtor.val, -1.7425E+05, places=13)
+        self.assertEqual(v.gamma.val, 0.0)
+        self.assertEqual(v.ncurr, 1)
+        self.assertFalse(v.free_boundary)
         self.assertEqual(v.status, "setup")
 
 if __name__ == "__main__":
--- mattopt/vmec.py
+++ mattopt/vmec.py
@@ -18,6 +18,16 @@ class Vmec:
         self.stelsym = Parameter(True)
         self.mpol = Parameter(1, min=1)
         self.ntor = Parameter(0, min=0)
+        self.delt = Parameter(0.7, min=0, max=1)
+        self.tcon0 = Parameter(2.0)
+        self.phiedge = Parameter(1.0)
+        self.curtor = Parameter(0.0)
+        self.gamma = Parameter(0.0)
+        self.boundary = SurfaceRZ(nfp=self.nfp.val, stelsym=self.stelsym.val, \
+                                      mpol=self.mpol.val, ntor=self.ntor.val)
+        # Handle a few variables that are not Parameters:
+        self.ncurr = 1
+        self.free_boundary = False
         self.status = "setup"
 
     def __repr__(self):
@@ -28,7 +38,8 @@ class Vmec:
             str(self.mpol.val) + " ntor=" + str(self.ntor.val) + " status=" + \
             self.status + ")"
 
-    def parse_namelist_var(self, dict, var, default, min=np.NINF, max=np.Inf):
+    def parse_namelist_var(self, dict, var, default, min=np.NINF, max=np.Inf, \
+                               new_name=None, parameter=True):
         """
         This method is used to streamline from_input_file(), and would
         not usually be called by users.
@@ -37,12 +48,22 @@ class Vmec:
             val_to_use = dict[var]
         else:
             val_to_use = default
-        setattr(self, var, Parameter(val_to_use, min=min, max=max))
+
+        if new_name is None:
+            name = var
+        else:
+            name = new_name
+
+        if parameter:
+            setattr(self, name, Parameter(val_to_use, min=min, max=max))
+        else:
+            setattr(self, name, val_to_use)
 
     @classmethod
     def from_input_file(cls, filename):
         """
-        Read in a VMEC input namelist.
+        Create an instance of the Vmec class based on settings that
+        are read in from a VMEC input namelist.
         """
         vmec = cls()
 
@@ -52,12 +73,23 @@ class Vmec:
         vmec.parse_namelist_var(dict, "nfp", 1, min=1)
         vmec.parse_namelist_var(dict, "mpol", 1, min=1)
         vmec.parse_namelist_var(dict, "ntor", 0, min=0)
+        vmec.parse_namelist_var(dict, "delt", 0.7)
+        vmec.parse_namelist_var(dict, "tcon0", 2.0)
+        vmec.parse_namelist_var(dict, "phiedge", 1.0)
+        vmec.parse_namelist_var(dict, "curtor", 0.0)
+        vmec.parse_namelist_var(dict, "gamma", 0.0)
+        vmec.parse_namelist_var(dict, "lfreeb", False, \
+                                    new_name="free_boundary", parameter=False)
+        vmec.parse_namelist_var(dict, "ncurr", 1, parameter=False)
 
-        # Handle lasym separately since we actually store its opposite.
+        # Handle a few variables separately:
         if "lasym" in dict:
             lasym = dict["lasym"]
         else:
             lasym = False
         vmec.stelsym = Parameter(not lasym)
+
+        vmec.boundary = SurfaceRZ(nfp=vmec.nfp.val, stelsym=vmec.stelsym.val, \
+                                      mpol=vmec.mpol.val, ntor=vmec.ntor.val)
 
         return vmec
